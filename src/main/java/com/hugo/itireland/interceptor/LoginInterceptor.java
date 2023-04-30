08f1ff48ca0196be8d6f6d0cd8b148e00308af64
PengYu1990 <yp1990@gmail.com>
2023-04-30 18:48:22 +0100

    Front End: Back End: Return sessionId Modify Login Interceptor

--- src/main/java/com/hugo/itireland/interceptor/LoginInterceptor.java
+++ src/main/java/com/hugo/itireland/interceptor/LoginInterceptor.java
@@ -7,24 +7,23 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-@Component
 public class LoginInterceptor implements HandlerInterceptor {
 
         @Override
         public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
             ServletContext context = request.getSession().getServletContext();
             String sessionId = request.getParameter("sessionId");
-            HttpSession session = (HttpSession)context.getAttribute(sessionId);
-            if(session != null) {
-                return true;
+
+            if(sessionId == null || context.getAttribute(sessionId) == null) {
+                R r = R.error(400,"You need to log in");
+                ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+                String json = ow.writeValueAsString(r);
+                response.getWriter().write(json);
+                return false;
             }
-            R r = R.error(400,"You need to log in");
-            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-            String json = ow.writeValueAsString(r);
-            response.getWriter().write(json);
-            return false;
+
+            return true;
         }
     }
