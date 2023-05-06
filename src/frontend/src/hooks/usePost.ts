e5e3ce498dc482cf009579deda612587a8619410
PengYu1990 <yp1990@gmail.com>
2023-05-06 12:48:20 +0100

    Remove frontend

--- src/frontend/src/hooks/usePost.ts
+++
@@ -1,5 +0,0 @@
-import { Post } from "./usePosts";
-import useSingleData from "./useSingleData";
-
-const usePost = (id:string|undefined) => useSingleData<Post>("/api/posts/"+id)
-export default usePost
