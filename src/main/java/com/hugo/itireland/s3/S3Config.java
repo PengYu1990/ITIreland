abdcc5b115de866cbc79ac2c99811ea61d5c32c5
PengYu1990 <yp1990@gmail.com>
2023-06-29 21:02:49 +0100

    config bucketteer keys

--- src/main/java/com/hugo/itireland/s3/S3Config.java
+++ src/main/java/com/hugo/itireland/s3/S3Config.java
@@ -3,6 +3,10 @@ package com.hugo.itireland.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.CredentialUtils;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -11,6 +15,12 @@ public class S3Config {
 
     @Value("${aws.region}")
     private String awsRegion;
+
+    @Value("accessKeyId")
+    private String accessKeyId;
+
+    @Value("secretAccessKey")
+    private String secretAccessKey;
 
     @Value("${aws.s3.mock}")
     private boolean mock;
@@ -21,6 +31,12 @@ public class S3Config {
             return new FakeS3();
         }
         return S3Client.builder()
+                .credentialsProvider(new AwsCredentialsProvider() {
+                    @Override
+                    public AwsCredentials resolveCredentials() {
+                        return AwsBasicCredentials.create("test", "test");
+                    }
+                })
                 .region(Region.of(awsRegion))
                 .build();
     }
