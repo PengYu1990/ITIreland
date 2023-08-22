d152c521f5fbed5defddef232428cdf37b4f07d1
PengYu1990 <yp1990@gmail.com>
2023-08-22 11:43:53 +0100

    Modify folder structure

---
+++ src/main/java/com/hugo/itireland/ItIrelandApplication.java
@@ -0,0 +1,15 @@
+package com.hugo.itireland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItIrelandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItIrelandApplication.class, args);
+
+    }
+
+
+}
